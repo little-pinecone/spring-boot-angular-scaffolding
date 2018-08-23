ed4c7fe5127deaeffc81fe1012d9bca7a7038cee
little_pinecone <marta.szymek.mail@gmail.com>
2018-08-23 12:46:18 +0200

    Manage the Spring Boot structure

---
+++ backend/src/main/java/in/keepgrowing/springbootangularscaffolding/SpringBootAngularScaffoldingApplication.java
@@ -0,0 +1,12 @@
+package in.keepgrowing.springbootangularscaffolding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularScaffoldingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularScaffoldingApplication.class, args);
+	}
+}
