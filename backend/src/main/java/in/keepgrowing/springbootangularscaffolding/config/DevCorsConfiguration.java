57086345f4cba273ff12db3965ff094f565f5174
little_pinecone <marta.szymek.mail@gmail.com>
2018-09-14 19:02:02 +0200

    Resource shared across Spring Boot API and Angular frontend

---
+++ backend/src/main/java/in/keepgrowing/springbootangularscaffolding/config/DevCorsConfiguration.java
@@ -0,0 +1,16 @@
+package in.keepgrowing.springbootangularscaffolding.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("development")
+public class DevCorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+    }
+}
